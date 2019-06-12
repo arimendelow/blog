4a53df9f4679ef2700694d33dbdbb599185229c5
Ari Mendelow <ari@mendelow.me>
2019-06-12 16:13:25 -0500

    Add some requirements for the articles submitted

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+	validates :title, presence: true, length: { minimum: 5 } # Make sure that all articles have a title that is at least 5 characters long
+	# for more about validations, see here: https://guides.rubyonrails.org/active_record_validations.html
 end

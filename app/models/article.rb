c153a3bf85313bcaa5097883c405dedfe12b65be
Ari Mendelow <ari@mendelow.me>
2019-06-13 14:44:14 -0500

    Add relationship between an article and comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
 	validates :title, presence: true, length: { minimum: 5 } # Make sure that all articles have a title that is at least 5 characters long
 	# for more about validations, see here: https://guides.rubyonrails.org/active_record_validations.html
+
+	# Relationship from Article to Comment: one article can have many comments
+	has_many :comments
 end
